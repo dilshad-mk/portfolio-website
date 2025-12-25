437149a2cbec7f5d36f93059c91bf32513365028
dilshad-mk <diludilshad1001@gmail.com>
2025-12-25 16:10:41 +0530

    portfolio website home section

--- script.js
+++ script.js
@@ -38,3 +38,21 @@ function typeEffect() {
 }
 
 typeEffect();
+
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.2
+  }
+);
+
+document.querySelectorAll(".scroll-animate").forEach(el => {
+  observer.observe(el);
+});
